ee23d7d592d39b3ade47f6c3968562e90d602790
Sangram <sangram15.behera@gmail.com>
2022-04-05 04:24:46 +0530

    Second Commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+   	public static void main(String[] argv) {
+			while(true) {
+             System.out.println("Hellow to Terminal Commands");
+		       try {		   
+               Thread.sleep(60000);	
+					} catch (Exception ex) {}
+	       }
+     }
+}
